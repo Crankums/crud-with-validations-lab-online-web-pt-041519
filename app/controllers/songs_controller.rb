aae954d6eb5226347ff60a715f825a48318e404a
Craig Donavin <crdonavin@gmail.com>
2019-09-17 21:44:08 +0000

    Automatically backed up by Learn

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,52 @@
+class SongsController < ApplicationController
+  before_action :set_song
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    set_song
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    set_song
+  end
+
+  def update
+    set_song
+    if @song.update(song_params)
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_song
+    @song = Song.find(params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit(
+    :title, :released, :release_year, :artist_name, :genre
+    )
+  end
+
+end
